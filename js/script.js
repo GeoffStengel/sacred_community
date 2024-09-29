f9d533ba7dcbe66781601b5311b389fa59d1270a
unknown <geoffsglass@hotmail.com>
2024-09-28 16:32:02 -0800

    Created a Templates folder for html files to keep organized, moved them all except index.html so it stays in root available for browsers to easily find and other applications easy access.  Added nav bar and hamburger button to all pages.  Nav bar has hover effects, a bit of animation and responsive layout. also added more js files script.js has hamburger button for nav

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const navItems = document.querySelectorAll('.nav-links li');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active');
+    hamburger.classList.toggle('toggle');
+});
